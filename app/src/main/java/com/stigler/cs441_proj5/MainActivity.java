74a659c60bd69b8d204b81e2c66c5f512d280b8e
stigglesworth3 <Kyle@Stigler.com>
2021-03-18 18:03:22 -0400

    was gonna do text to speech but ended up creating a music player instead

--- app/src/main/java/com/stigler/cs441_proj5/MainActivity.java
+++ app/src/main/java/com/stigler/cs441_proj5/MainActivity.java
@@ -1,14 +1,47 @@
 package com.stigler.cs441_proj5;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity
+{
+    TextView inputLabel;
+    Button stopButton;
+    MediaPlayer mPlayer;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputLabel = (TextView) findViewById(R.id.inputLabel);
+        stopButton = (Button) findViewById(R.id.stopButton);
+
+
+    }
+
+    public void startPlaying(View view)
+    {
+        mPlayer = MediaPlayer.create(this, R.raw.testsong);
+        mPlayer.start();
+    }
+
+    public void stopPlaying(View view)
+    {
+        mPlayer.stop();
     }
 }
